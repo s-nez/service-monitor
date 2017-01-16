06ba46a6120a36e96e53f46ca5975342f84fbf82
Szymon Nieznański <s.nez@member.fsf.org>
2017-01-16 01:02:36 +0100

    Add email to copyright notices

--- SimpleLabel.py
+++ SimpleLabel.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017  Szymon Nieznański
+# Copyright (C) 2017  Szymon Nieznański (s.nez@member.fsf.org)
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
--- monitor.py
+++ monitor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2017  Szymon Nieznański
+# Copyright (C) 2017  Szymon Nieznański (s.nez@member.fsf.org)
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
